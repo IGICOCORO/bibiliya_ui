7da3c9a5606778bead5d8e741a606d4e4aa9aeb5
IGICOCORO <nduwimanaguymich@gmail.com>
2022-11-21 19:30:13 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bibiliya_nyeranda',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
